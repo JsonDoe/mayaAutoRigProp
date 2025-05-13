edab391461acb9c12ad82b9340b816f37dc6b93c
Julien MITERNIQUE <julien.miternique@piktura.fr>
2025-05-13 17:16:28 +0200

    fix main

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ tool_path = r"C:/Users/julien.miternique/Documents/workspace/mayaAutoRigProp"
 if tool_path not in sys.path:
     sys.path.append(tool_path)
 
-# Reload both modules
-import logic.auto_rig_script_update
+# Reload module
+import logic.auto_rig_script
 
-importlib.reload(logic.auto_rig_script_update)
+importlib.reload(logic.auto_rig_script)
 
-from logic.auto_rig_script_update import auto_rig_prop
+from logic.auto_rig_script import auto_rig_prop
 
 auto_rig_prop()
